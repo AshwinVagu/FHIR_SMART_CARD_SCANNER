5fd86639e60b2d531bdc7aa0cbc39e4400121785
Ashwin Sai Vagu <vagua01@pfw.edu>
2025-01-15 08:29:41 -0500

    added api and ux

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ScanQrCode extends StatefulWidget {
   const ScanQrCode({super.key});
@@ -11,6 +13,7 @@ class ScanQrCode extends StatefulWidget {
 
 class _ScanQrCodeState extends State<ScanQrCode> {
   String qrResult = 'Scanned Data will appear here';
+  bool isLoading = false;
 
   Future<void> scanQR()async{
     try{
@@ -18,9 +21,52 @@ class _ScanQrCodeState extends State<ScanQrCode> {
       if(!mounted){
         return;
       }
-      setState(() {
-        this.qrResult = qrCode.toString();
-      });
+        if(qrCode.toString().contains("shc:/")){
+          final url = Uri.parse('https://fhir-smart-card-scanner.onrender.com/qr-decoding'); // Example API endpoint
+          final headers = {'Content-Type': 'application/json'};
+          final payload = {
+            'qr_result': qrCode.toString()
+          };
+          try {
+            setState(() {
+              isLoading = true;
+            });
+            final response = await http.post(
+              url,
+              headers: headers,
+              body: json.encode(payload), // Convert the payload to JSON
+            );
+            setState(() {
+              isLoading = false;
+              if (response.statusCode == 201) {
+                final final_body = json.decode(response.body);
+                String medical_entries = "";
+                final entries = final_body['result'][1]['vc']['credentialSubject']['fhirBundle']['entry'];
+                for (var entry in entries.asMap().entries) {
+                  if(entry.key!=0){
+                    medical_entries = medical_entries + json.encode(entry.value) + '\n';
+                  }
+                }
+                this.qrResult = "Name details: ${entries[0]['resource']['name']} \n Type: ${entries[0]['resource']['resourceType']} \n Medical Entries: ${medical_entries} \n Verified: ${final_body['verified'].toString()}";
+              } else {
+                this.qrResult = "Failed to get data";
+              }
+            });
+          } catch (e) {
+            print('Error occurred: $e');
+            setState(() {
+              isLoading = false;
+              this.qrResult = "Failed to get data";
+            });
+          }
+        }
+        else{
+          setState(() {
+            this.qrResult = "Not a smart card associated QR.";
+          });
+        }
+
+
     }on PlatformException{
       qrResult = 'Failed to read QR code';
     }
@@ -31,15 +77,24 @@ class _ScanQrCodeState extends State<ScanQrCode> {
     return Scaffold(
       appBar: AppBar(title: Text('QR Code scanner')),
       body: Center(
-        child: Column(
+        child: SingleChildScrollView(
+          child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(height: 30,),
-            Text('$qrResult',style: TextStyle(color: Colors.black),),
+            Container(
+                margin: EdgeInsets.all(16.0),
+                child: Text('$qrResult',style: TextStyle(color: Colors.black),)
+            ),
             SizedBox(height: 30,),
-            ElevatedButton(onPressed: scanQR, child: Text('Scan Code'))
+            ElevatedButton(onPressed: scanQR, child: Text('Scan Code')),
+            if(isLoading) CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.black), // Spinner color
+              strokeWidth: 6.0,
+            )
           ],
         ),
+        )
       ),
     );
   }
